2a965a55666f47c567250641dbbb66e34991da7e
Piyashhasan <piyashhasan38@gmail.com>
2024-10-16 21:36:33 +0600

    feat: Project Setup + Start - done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
