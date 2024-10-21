c6107eb750d8aadbb27dbc779030eb0658282fed
Piyashhasan <piyashhasan38@gmail.com>
2024-10-21 14:47:57 +0600

    feat: Home page responsive - done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,11 +12,11 @@ const Home = () => {
   return (
     <div className="wrapper">
       <div className="flex flex-wrap items-center justify-between px-4 lg:flex-nowrap xl:px-0">
-        <div className="w-full text-center mt-[50px] lg:text-left lg:mt-0 ">
+        <div className="w-full text-center mt-[50px] lg:text-left lg:mt-0">
           <p className="text-[#2FCD71] text-[18px] sm:text-[20px] font-bold leading-7">
             Books Gallery
           </p>
-          <h1 className="text-[32px] sm:text-[40px] font-semibold leading-[50px] my-5 sm:my-6 lg:py-8">
+          <h1 className="text-[32px] sm:text-[40px] font-semibold leading-[50px] my-5 sm:my-6 lg:py-8 lg:my-0">
             Discover Your Next <br className="hidden sm:block" /> Great Read
           </h1>
           <p className="text-[18px]">
@@ -28,8 +28,8 @@ const Home = () => {
             </button>
           </Link>
         </div>
-        <div className="w-full flex items-center justify-center lg:justify-end mt-14 lg:mt-0">
-          <div className="w-[500px]">{View}</div>
+        <div className="w-full flex items-center justify-center lg:justify-end mt-14 lg:mt-3">
+          <div className="max-w-[500px]">{View}</div>
         </div>
       </div>
     </div>
