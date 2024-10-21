56d8403fcbcdf88cc8554bd727a228cbde5cff56
Piyashhasan <piyashhasan38@gmail.com>
2024-10-21 15:57:21 +0600

    feat: Active nav link add

--- src/shared/NavBar/NavBar.jsx
+++ src/shared/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/images/icon.png";
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -8,51 +8,76 @@ const NavBar = () => {
   const [toggleMobileMenu, setToggleMobileMenu] = useState(false);
 
   return (
-    <nav className="wrapper">
-      <div className="flex items-center justify-between py-[10px] px-4 xl:px-0">
-        {/* --- logo side start --- */}
-        <Link to="/">
-          <div className="flex items-center gap-3">
-            <div>
-              <img className="w-[50px] h-auto" src={logo} alt="logo" />
+    <nav className="sticky top-0 bg-white z-20">
+      <div className="wrapper">
+        <div className="flex items-center justify-between py-[10px] px-4 xl:px-0">
+          {/* --- logo side start --- */}
+          <Link to="/">
+            <div className="flex items-center gap-3">
+              <div>
+                <img className="w-[50px] h-auto" src={logo} alt="logo" />
+              </div>
+              <h3 className="text-[28px] font-semibold text-[#2FCD71]">
+                Books
+              </h3>
             </div>
-            <h3 className="text-[28px] font-semibold text-[#2FCD71]">Books</h3>
+          </Link>
+          {/* --- logo side end --- */}
+
+          {/* --- nav link start --- */}
+          <div className="hidden sm:block">
+            <ul className="flex items-center gap-10 text-[20px] font-semibold text-[#2FCD71]">
+              <li className="hover:text-green-600">
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive ? "border-b-2 border-green-500" : ""
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="hover:text-green-600">
+                <NavLink
+                  to="/books"
+                  className={({ isActive }) =>
+                    isActive ? "border-b-2 border-green-500" : ""
+                  }
+                >
+                  Books
+                </NavLink>
+              </li>
+              <li className="hover:text-green-600">
+                <NavLink
+                  to="/wishlist"
+                  className={({ isActive }) =>
+                    isActive ? "border-b-2 border-green-500" : ""
+                  }
+                >
+                  Wishlist
+                </NavLink>
+              </li>
+            </ul>
           </div>
-        </Link>
-        {/* --- logo side end --- */}
+          {/* --- nav link end --- */}
 
-        {/* --- nav link start --- */}
-        <div className="hidden sm:block">
-          <ul className="flex items-center gap-10 text-[20px] font-semibold text-[#2FCD71]">
-            <li className="hover:text-green-600">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="hover:text-green-600">
-              <Link to="/books">Books</Link>
-            </li>
-            <li className="hover:text-green-600">
-              <Link to="/wishlist">Wishlist</Link>
-            </li>
-          </ul>
+          {/* --- toggle button --- */}
+          <div
+            className="sm:hidden"
+            onClick={() => setToggleMobileMenu(!toggleMobileMenu)}
+          >
+            <GiHamburgerMenu className="text-3xl cursor-pointer text-[#0d0e43" />
+          </div>
+          {/* --- toggle button --- */}
         </div>
-        {/* --- nav link end --- */}
 
-        {/* --- toggle button --- */}
-        <div
-          className="sm:hidden"
-          onClick={() => setToggleMobileMenu(!toggleMobileMenu)}
-        >
-          <GiHamburgerMenu className="text-3xl cursor-pointer text-[#0d0e43" />
-        </div>
-        {/* --- toggle button --- */}
+        {/* --- mobile menu start --- */}
+        <MobileNav
+          toggleMobileMenu={toggleMobileMenu}
+          setToggleMobileMenu={setToggleMobileMenu}
+        />
+        {/* --- mobile menu end --- */}
       </div>
-
-      {/* --- mobile menu start --- */}
-      <MobileNav
-        toggleMobileMenu={toggleMobileMenu}
-        setToggleMobileMenu={setToggleMobileMenu}
-      />
-      {/* --- mobile menu end --- */}
     </nav>
   );
 };
