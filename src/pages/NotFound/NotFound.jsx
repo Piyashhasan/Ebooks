67e580ec0807ee88623ff89cd1839f4f1786b5cb
Piyashhasan <piyashhasan38@gmail.com>
2024-10-21 16:19:33 +0600

    feat: Not found page - done

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
-    <div>
-      <h1>NotFound page</h1>
+    <div className="text-center my-16">
+      <h2 className="my-5 text-[18px] font-bold">Not Found</h2>
+      <p className="font-bold mb-5">Could not find requested resource</p>
+      <Link className="bg-[#2FCD71] px-10 py-2 rounded-md text-white" to="/">
+        Return Home
+      </Link>
     </div>
   );
 };
